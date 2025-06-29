386bce2c0a8adc8eece438d749d6dca202b4b272
GhadaFts <ghadafatnassi49@gmail.com>
2025-06-29 23:08:33 +0100

    design changement

--- frontend/src/components/PrevTopicsCarousel.vue
+++ frontend/src/components/PrevTopicsCarousel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="carousel-wrapper">
     <div class="section-title">
-      <h2 class="carousel-title">Main Topics Of The Conference</h2>
+      <h2 class="carousel-title">Main Topics Of The Conference </h2>
     </div>
 
     <button @click="prev" class="nav-button left" :disabled="currentIndex === 0">
@@ -117,15 +117,15 @@ function prev() {
   content: "";
   flex: 1;
   height: 1px;
-  background-color: #0c5fa6;
+  background-color: #265985;
   margin: 0 1rem;
 }
 
 .section-title h2 {
   font-size: 1.2rem;
-  color: #0c5fa6;
+  color: #265985;
   font-weight: bold;
-  border: 1px solid #0c5fa6;
+  border: 1px solid #265985;
   border-radius: 999px;
   padding: 0.5rem 2rem;
   background-color: #fff;
@@ -146,7 +146,7 @@ function prev() {
   font-size: 1.3rem;
   font-weight: bold;
   margin-bottom: 2rem;
-  color: #1f2937;
+  color: #265985;
 }
 
 .carousel-container {
@@ -176,7 +176,7 @@ function prev() {
 .custom-line {
   width: 96%;
   height: 1px;
-  background-color: #003f7f;
+  background-color: #265985;
   border: none;
   position: relative;
 }
@@ -186,7 +186,7 @@ function prev() {
   content: '';
   position: absolute;
   height: 1px;
-  background-color: #003f7f;
+  background-color: #265985;
   width: 50px;
   top: 0;
 }
